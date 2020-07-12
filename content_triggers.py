94f4bc4383f66011586c155b46b0268370c4ac89
CrazyIlian <ilian04@mail.ru>
2020-07-12 23:53:53 +0300

    Added inline query

---
+++ content_triggers.py
@@ -0,0 +1,36 @@
+import ast
+import re
+
+
+def calc_loaded_triggers(id2triggers):
+    triggers2id = dict()
+    for id in id2triggers:
+        for trigger in id2triggers[id]:
+            triggers2id[trigger] = id
+    return id2triggers, triggers2id
+
+
+def load_triggers():
+    with open('triggers.txt') as f:
+        id2triggers = ast.literal_eval(f.read())
+    return id2triggers
+
+
+def dump_triggers(id2triggers):
+    with open('triggers.txt') as f:
+        f.write(str(id2triggers))
+
+
+def get_text_from_id(file_id):
+    return file_id[1:]
+
+
+def check_url(url):
+    regex = re.compile(
+        r'^(?:https?://|)'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(regex, url) is not None
+
---
+++ main.py
@@ -0,0 +1,89 @@
+import telebot
+from content_triggers import *
+
+
+with open('secrets/token.txt') as f:
+    bot = telebot.TeleBot(f.read().strip())
+
+id2triggers, triggers2id = calc_loaded_triggers(load_triggers())
+
+
+def get_variants(trig):
+    variants = []
+    for trigger in triggers2id:
+        ind = trigger.find(trig)
+        if ind == -1:
+            continue
+        variants.append((ind != 0, len(trigger), trigger))
+    variants.sort()
+    return [var[-1] for var in variants]
+
+
+def get_content_type(file_id):
+    if file_id[0] == '#':
+        text = get_text_from_id(file_id)
+        return ('text', 'link')[check_url(text)]
+    dir = bot.get_file(file_id).file_path.split('/')[0]
+    typ = {'photos': 'photo',
+           'animations': 'gif',
+           'stickers': 'sticker',
+           'videos': 'video'}.get(dir, dir)
+    return typ
+
+
+def get_inline_content(trig, index):
+    file_id = triggers2id[trig]
+    typ = get_content_type(file_id)
+    if typ == 'photo':
+        return telebot.types.InlineQueryResultCachedPhoto(index, file_id, trig)
+    elif typ == 'video':
+        return telebot.types.InlineQueryResultCachedVideo(index, file_id, trig)
+    elif typ == 'gif':
+        return telebot.types.InlineQueryResultCachedGif(index, file_id, trig)
+    elif typ == 'sticker':
+        return telebot.types.InlineQueryResultCachedSticker(index, file_id)
+    elif typ == 'text':
+        thumb = 'https://i.imgur.com/O2yehMw.png'
+        text = get_text_from_id(file_id)
+        message = telebot.types.InputTextMessageContent(
+            text, parse_mode='Markdown',
+            disable_web_page_preview=False)
+        return telebot.types.InlineQueryResultArticle(index, trig, message,
+                                                      description=text,
+                                                      thumb_url=thumb)
+    elif typ == 'link':
+        thumb = 'https://i.imgur.com/tokK703.png'
+        link = get_text_from_id(file_id)
+        message = telebot.types.InputTextMessageContent(
+            link, parse_mode='Markdown',
+            disable_web_page_preview=trig in ('rickroll', 'рикролл'))
+        return telebot.types.InlineQueryResultArticle(index, trig, message,
+                                                      url=link,
+                                                      hide_url=False,
+                                                      thumb_url=thumb)
+    else:
+        print('Unknown content type:', typ)
+        print('Trigger:', trig)
+
+
+@bot.inline_handler(func=lambda query: len(query.query) > 0)
+def inline(query):
+    trig = query.query.lower()
+    variants = get_variants(trig)
+    if len(variants) == 0:
+        variants.append('триггер не найден')
+    content = []
+    content_file_ids = set()
+    for trigger in variants:
+        if len(content) == 20:
+            break
+        file_id = triggers2id[trigger]
+        if file_id in content_file_ids:
+            continue
+        content_file_ids.add(file_id)
+        content.append(get_inline_content(trigger, len(content)))
+    return bot.answer_inline_query(query.id, content)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling(timeout=10)
